643d8d6d54b32133a15a24402156c23eb1675fe7
Mạnh Hung <nmhung0519@gmail.com>
2021-05-09 15:45:52 +0700

    Thêm màn Home Fragment

--- app/src/main/java/android/example/smartmeal/Common.kt
+++ app/src/main/java/android/example/smartmeal/Common.kt
@@ -2,6 +2,6 @@ package android.example.smartmeal
 
 class Common {
     companion object {
-        val DOMAIN: String = "http://192.168.1.190"
+        val DOMAIN: String = "http://192.168.2.12"
     }
 }
--- app/src/main/java/android/example/smartmeal/table/FragmentTable.kt
+++ app/src/main/java/android/example/smartmeal/table/FragmentTable.kt
@@ -7,31 +7,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.RecyclerView
 
 
 class FragmentTable : Fragment() {
-
-    private val tableViewModel by viewModels<TableViewModel> {
-        TableViewModelFactory(context as Context)
-    }
-
+    private var token: String? = null
+    private lateinit var tableViewModel: TableViewModel
+    private var loadingPanel: RelativeLayout? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        token = arguments?.getString("token")
+        tableViewModel = ViewModelProviders.of(this, TableViewModelFactory(context as Context, token)).get(TableViewModel::class.java)
         var view = inflater.inflate(R.layout.fragment_table, container, false)
-
+        loadingPanel = view?.findViewById(R.id.loadingPanel)
+        onLoading()
         val tableAdapter = TableAdapter { flower -> adapterOnClick(flower) }
-
         view?.findViewById<RecyclerView>(R.id.recyclerView)?.adapter = tableAdapter
 
         tableViewModel.tables.observe(this.requireActivity(), Observer {
@@ -39,11 +41,24 @@ class FragmentTable : Fragment() {
                 tableAdapter.submitList(it as MutableList<TableModel>)
             }
         })
-        return view;
+        tableAdapter.isDoneFirstLoad.observe(this.requireActivity(), Observer {
+            it?.let {
+                if (it) loaded()
+            }
+        })
+        return view
+    }
+
+    fun onLoading() {
+        loadingPanel?.visibility = View.VISIBLE
+    }
+
+    fun loaded() {
+        loadingPanel?.visibility = View.GONE
     }
 
     fun adapterOnClick(table: TableModel) {
-        Toast.makeText(context, table.tableName, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, table.TableName, Toast.LENGTH_SHORT).show()
     }
 
     fun InsertTable() {
